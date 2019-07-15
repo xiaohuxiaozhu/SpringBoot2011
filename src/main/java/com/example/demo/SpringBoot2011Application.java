fb25937aa608484f0415c8411e23ed9356ad4f8a
togoboy8 <cedrichillah@gmail.com>
2019-07-15 13:05:58 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot2011Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2011Application.class, args);
+	}
+
+}
